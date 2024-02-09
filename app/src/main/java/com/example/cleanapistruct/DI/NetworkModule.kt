e501e30662af3646e2b8e8dfd31a1a65fdd989f9
nikoloz gabashvili <nika2005nika2005@gmail.com>
2024-02-09 11:24:57 +0400

    logging 👍👍👌

--- app/src/main/java/com/example/cleanapistruct/DI/NetworkModule.kt
+++ app/src/main/java/com/example/cleanapistruct/DI/NetworkModule.kt
@@ -20,7 +20,7 @@ import javax.inject.Singleton
 object NetworkModule {
 
 
-    @Singleton
+
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY // Set log level as needed
     }
